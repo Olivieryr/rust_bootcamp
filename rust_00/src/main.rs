f25b47a6460fd0bb5ac8bba3ad526ef9448806de
Olivieryr <olivieryerro97212@gmail.com>
2025-11-25 16:29:24 +0100

    Adding repeat feature

--- rust_00/src/main.rs
+++ rust_00/src/main.rs
@@ -1,10 +1,31 @@
 use std::env;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let name=args.iter().skip(1).find(|arg| !arg.starts_with("--")).map(|arg|arg.as_str());  
+    let args: Vec<String> = env::args().collect();  
     let upper = args.iter().any(|arg| arg=="--upper");  
-    hello(name,upper);
+    let pos=args.iter().position(|arg| arg=="--repeat");
+    let repeat = match pos{
+        Some(pos)=>{
+            if pos+1 <args.len(){
+                match args[pos+1].parse::<usize>(){
+                    Ok(n)=>n,
+                    Err(_)=>{
+                        eprintln!("Erreur: --repeat doit être suivi d'un nombre");
+                        return;
+                    }
+                }
+            }
+            else{
+                eprintln!("Erreur: --repeat doit être suivi d'un nombre");
+                return;
+            }
+        }
+        None=>1,
+    };
+    let name=args.iter().enumerate().skip(1).find(|(i,arg)| !arg.starts_with("--")&&Some(*i)!=pos.map(|p| p+1)).map(|(_i,arg)|arg.as_str());
+    for _ in 0..repeat{
+            hello(name,upper);
+    }
 }
 fn hello(name: Option<&str>,upper:bool){
     let mut message = match name{
