81059eb360015eb2e614d63a875308a92fb7e3d3
Olivieryr <olivieryerro97212@gmail.com>
2025-11-30 18:44:10 +0100

    added min argument

--- rust_01/src/main.rs
+++ rust_01/src/main.rs
@@ -1,11 +1,13 @@
 use std::collections::HashMap;
 use std::io;
 use clap::{Arg, Command, ArgAction};
-fn compte_top(phrase:String,top:usize){
+fn compte_top(phrase:String,top:usize,min:usize){
     let phrase = phrase.to_lowercase();
     let mut compteur = HashMap::new();
     for mot in phrase.split_whitespace() {
-        *compteur.entry(mot.to_string()).or_insert(0) += 1;
+        if mot.len()>=min{
+            *compteur.entry(mot.to_string()).or_insert(0) += 1;
+        }
     }
     let mut classement: Vec<(String,u32)> = compteur.into_iter().collect();
     classement.sort_by(|a,b|b.1.cmp(&a.1));
@@ -40,11 +42,19 @@ fn main() {
             .default_value("0")
             .value_parser(clap::value_parser!(usize)),
         )
+        .arg(
+            Arg::new("min-length")
+            .long("min")
+            .help("taille minimum pour être compté")
+            .default_value("1")
+            .value_parser(clap::value_parser!(usize)),
+        )
         .get_matches();
+        let min=*matches.get_one::<usize>("min-length").unwrap();
         let top=*matches.get_one::<usize>("top").unwrap();
         let phrase=matches
             .get_one::<String>("phrase")
             .unwrap()
             .clone();
-        compte_top(phrase,top);
+        compte_top(phrase,top,min);
 }
