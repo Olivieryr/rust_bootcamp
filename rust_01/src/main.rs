b286d616d99765990a9ca7383612a6cb284cf3fc
Olivieryr <olivieryerro97212@gmail.com>
2025-11-30 17:33:06 +0100

    added top argument

--- rust_01/src/main.rs
+++ rust_01/src/main.rs
@@ -1,16 +1,26 @@
 use std::collections::HashMap;
 use std::io;
 use clap::{Arg, Command, ArgAction};
-fn compte(phrase:String){
+fn compte_top(phrase:String,top:usize){
     let phrase = phrase.to_lowercase();
     let mut compteur = HashMap::new();
     for mot in phrase.split_whitespace() {
         *compteur.entry(mot.to_string()).or_insert(0) += 1;
     }
+    let mut classement: Vec<(String,u32)> = compteur.into_iter().collect();
+    classement.sort_by(|a,b|b.1.cmp(&a.1));
     println!("Occurrences des mots :");
-    for (mot, nb) in compteur {
+    if top !=0{
+        for (mot, nb) in classement.iter().take(top) {
         println!("{} : {}", mot, nb);
+        }
     }
+    else{
+        for (mot, nb) in classement.iter() {
+        println!("{} : {}", mot, nb);
+        }
+    }
+
 }
 fn main() {
     let matches = Command::new("compteur_app")
@@ -27,12 +37,14 @@ fn main() {
             Arg::new("top")
             .long("top")
             .help("Affiche les n mots les plus présents")
+            .default_value("0")
             .value_parser(clap::value_parser!(usize)),
         )
         .get_matches();
+        let top=*matches.get_one::<usize>("top").unwrap();
         let phrase=matches
             .get_one::<String>("phrase")
             .unwrap()
             .clone();
-        compte(phrase);
+        compte_top(phrase,top);
 }
