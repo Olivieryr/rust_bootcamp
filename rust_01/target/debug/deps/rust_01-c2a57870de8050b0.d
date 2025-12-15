92b6cc08ace35f0fdb63e060966a9c0e2eaf1cda
Olivieryr <olivieryerro97212@gmail.com>
2025-12-15 10:09:35 +0100

    Everything works now

---
+++ rust_01/target/debug/deps/rust_01-c2a57870de8050b0.d
@@ -0,0 +1,9 @@
+C:\Users\olivi\rust_bootcamp\rust_01\target\debug\deps\rust_01-c2a57870de8050b0.d: src\main.rs Cargo.toml
+
+C:\Users\olivi\rust_bootcamp\rust_01\target\debug\deps\librust_01-c2a57870de8050b0.rmeta: src\main.rs Cargo.toml
+
+src\main.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__
+# env-dep:CLIPPY_CONF_DIR
